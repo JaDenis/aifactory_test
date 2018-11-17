8919ca17b6dd84db5e2c9829ccc512cf12ed7514
Denis Black <madaradess@gmail.com>
2018-11-17 21:09:34 +0500

    Initial Commit

---
+++ AIFactory_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AIFactory_test
+//
+//  Created by MacBook Air on 17/11/2018.
+//  Copyright © 2018 123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
